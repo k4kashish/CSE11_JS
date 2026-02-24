42ae1aead172b2a44bfca024f1b00be3b08c0871
k4kashish <k4kashish1108@gmail.com>
2026-02-24 09:45:55 +0530

    new commit

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-console.log("Hello World");
-document.write("Hello Woeld");
+
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="Kashish";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+//function
+var sayHello=function(){
+    alert('hello world!');
+}
+//array
+var numberArray=[1,2,3];
+var animals=new Array("cat","dog","mouse","lion");
+//object
+var person={
+    name:'Barack Hussein Obama II',
+    age:'51',
+    title:'44th President of the United States'
+}
+const sym1=Symbol(4)
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false")
+}
