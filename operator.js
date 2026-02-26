350deeb2476bddbdb6da6c7d275a3600c633a352
k4kashish <k4kashish1108@gmail.com>
2026-02-26 08:15:39 +0530

    operators

--- operator.js
+++ operator.js
@@ -1,1 +1,106 @@
-//assignment
+//Arithmetic operator
+var a=100; var b=13; var c=10; var linebreak="<br>";
+document.write("a+b+c=");
+result=a+b+c;
+document.write(result);
+document.write(linebreak);
+
+document.write("a+b=");
+result=a+b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a-b=");
+result=a-b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a/b=");
+result=a/b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a%b=");
+result=a%b;
+document.write(result);
+document.write(linebreak);
+
+
+//Comparison operator
+var a=20;
+var b=30;
+var linebreak="<br/>";
+
+document.write("(a==b)=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a<b)=>");
+result=(a<b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a>b)=>");
+result=(a>b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a!=b)=>");
+result=(a!=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a>=b)=>");
+result=(a>=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a<=b)=>");
+result=(a<=b);
+document.write(result);
+document.write(linebreak);
+
+//Assignment operators
+var a=10;
+var b=5;
+var linebreak="<br/>";
+
+document.write("Value of a=>(a=b)=>");
+result=(a=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Value of a=>(a+=b)=>");
+result=(a+=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Value of a=>(a-=b)=>");
+result=(a-=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Value of a=>(a*=b)=>");
+result=(a*=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Value of a=>(a/=b)=>");
+result=(a/=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Value of a=>(a%=b)=>");
+result=(a%=b);
+document.write(result);
+document.write(linebreak);
+
+
+
+
+
+
+
+
+
